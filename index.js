119f0901b4a3e09baf6acce440bbcaef0599c759
artur.farias <Biere#10>
2018-01-18 21:58:58 -0300

    Doygen API.

---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+    Command : require('./lib/command'),
+    Generator : require('./lib/generator'),
+    Port : require('./lib/port'),
+    Worker : require('./lib/worker')
+}
